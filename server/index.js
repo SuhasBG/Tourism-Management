db80a04799989b39fba8e4ab82408d00da60e3ba
Suhas B G <92786907+SuhasBG@users.noreply.github.com>
2022-01-24 14:20:03 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ if (port == null || port == "") {
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Suhasbg@2001",
+  password: "***********",
   database: "tourism",
 });
 
