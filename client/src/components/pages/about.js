950ede1c157d834ca3f531461883d0b7a53cd177
Suhas <suhasbg448@gmail.com>
2022-01-27 21:49:00 +0530

    About us

--- client/src/components/pages/about.js
+++ client/src/components/pages/about.js
@@ -2,26 +2,31 @@ import React from "react";
 
 const About = () => {
   return (
-    <div style={{ 
-      backgroundImage: `url("https://www.vtdesignz.com/wp-content/uploads/2016/06/about-us-banner.jpg")`,
-      backgroundSize: 'cover', 
-      backgroundPosition: 'center center',
-      backgroundRepeat: 'no-repeat',
-      paddingTop: '250px',
-      paddingBottom: '260px'  
-    }}>
+    <div
+      style={{
+        backgroundImage: `url("https://www.vtdesignz.com/wp-content/uploads/2016/06/about-us-banner.jpg")`,
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+        backgroundRepeat: "no-repeat",
+        paddingTop: "250px",
+        paddingBottom: "260px",
+      }}
+    >
       <div className="container">
-      <div className="py-4">
-        <h1 style={{color: 'white', marginBottom: '15px'}}>About Page</h1>
-        <p className="lead" style={{color: 'white', background: 'black', padding: '15px 0'}}>
-          This project uses OpenMaps API to locate and to calculate the distance
-          between the locations and provides the shortest distance between the
-          selected points.
-        </p>
-      </div>
+        <div className="py-4">
+          <h1 style={{ color: "white", marginBottom: "15px" }}>About Us</h1>
+          <p
+            className="lead"
+            style={{ color: "white", background: "black", padding: "15px 0" }}
+          >
+            This project uses OpenMaps API to locate and to calculate the
+            distance between the locations and provides the shortest distance
+            between the selected points. This project helps tourists to find
+            differnt tourist places near the city they are planning to visit.
+          </p>
+        </div>
       </div>
     </div>
-    
   );
 };
 
--- client/src/components/pages/contact.js
+++ client/src/components/pages/contact.js
@@ -2,19 +2,70 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div style={{ 
-      backgroundImage: `url("https://media.istockphoto.com/photos/communication-symbols-contact-info-symbols-picture-id1201366596?k=20&m=1201366596&s=612x612&w=0&h=6swaFuvPnPzJnKYqmbni8Gzbu7WFDBvWGguDFlamm4k=")`,
-      backgroundSize: 'cover', 
-      backgroundPosition: 'center center',
-      backgroundRepeat: 'no-repeat',
-      padding: '300px'  
-    }}>
+    <div
+      style={{
+        backgroundImage: `url("https://media.istockphoto.com/photos/communication-symbols-contact-info-symbols-picture-id1201366596?k=20&m=1201366596&s=612x612&w=0&h=6swaFuvPnPzJnKYqmbni8Gzbu7WFDBvWGguDFlamm4k=")`,
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+        backgroundRepeat: "no-repeat",
+        padding: "200px",
+      }}
+    >
       <div className Name="container">
-      <h1 style={{color: 'black', marginBottom: 0}}>Email : suhasbg448@gmail.com</h1>
-      <h1 style={{color: 'black', marginTop: 0}}>Phone : 8431551520</h1>
+        <h1 style={{ color: "black", marginBottom: 0 }}>
+          Suhas B G : suhasbg448@gmail.com
+        </h1>
+        <h1 style={{ color: "black", marginBottom: 0 }}>
+          Niranjan : nirudikshi27@gmail.com
+        </h1>
+        <h1 style={{ color: "black", marginBottom: 0 }}>
+          Sushanth S : raosushanth511@gmail.com
+        </h1>
+        <h1 style={{ color: "black", marginBottom: 0 }}>
+          Yogesh K M : murthyogesh@gmail.com
+        </h1>
+        <h1 style={{ color: "black", marginTop: 0 }}></h1>
       </div>
+
+      {/* <div className="container">
+        <table>
+          <tr>
+            <h1 style={{ color: "black", marginBottom: 0 }}>
+              <td>Suhas B G </td>
+              <td>
+                <a href="https://mail.google.com/mail/u/0/#inbox?compose=new">
+                  suhasbg448@gmail.com
+                </a>
+              </td>
+            </h1>
+          </tr>
+          <tr>
+            <td>Niranjan Dikshith T V </td>
+            <td>
+              <a href="https://mail.google.com/mail/u/0/#inbox?compose=new">
+                nirudikshi27@gmail.com
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>Sushanth S </td>
+            <td>
+              <a href="https://mail.google.com/mail/u/0/#inbox?compose=new">
+                raosushanth511@gmail.com
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>Yogesh K M </td>
+            <td>
+              <a href="https://mail.google.com/mail/u/0/#inbox?compose=new">
+                murthyogesh@gmail.com
+              </a>
+            </td>
+          </tr>
+        </table> */}
+      {/* </div> */}
     </div>
-    
   );
 };
 
