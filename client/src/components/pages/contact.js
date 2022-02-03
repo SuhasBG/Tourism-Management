cda5835f5d79306f846a5ae5f2d27ed581ec234a
Suhas B G <92786907+SuhasBG@users.noreply.github.com>
2022-02-03 12:37:57 +0530

    Update contact.js
    added different names

--- client/src/components/pages/contact.js
+++ client/src/components/pages/contact.js
@@ -13,58 +13,16 @@ const Contact = () => {
     >
       <div className Name="container">
         <h1 style={{ color: "black", marginBottom: 0 }}>
-          Suhas B G : suhasbg448@gmail.com
+          Devothama GN : devothamgn321@gmail.com
         </h1>
         <h1 style={{ color: "black", marginBottom: 0 }}>
-          Niranjan : nirudikshi27@gmail.com
+          Dheemanth R: dheemanth24@gmail.com
         </h1>
-        <h1 style={{ color: "black", marginBottom: 0 }}>
-          Sushanth S : raosushanth511@gmail.com
-        </h1>
-        <h1 style={{ color: "black", marginBottom: 0 }}>
-          Yogesh K M : murthyogesh@gmail.com
-        </h1>
+        
         <h1 style={{ color: "black", marginTop: 0 }}></h1>
       </div>
 
-      {/* <div className="container">
-        <table>
-          <tr>
-            <h1 style={{ color: "black", marginBottom: 0 }}>
-              <td>Suhas B G </td>
-              <td>
-                <a href="https://mail.google.com/mail/u/0/#inbox?compose=new">
-                  suhasbg448@gmail.com
-                </a>
-              </td>
-            </h1>
-          </tr>
-          <tr>
-            <td>Niranjan Dikshith T V </td>
-            <td>
-              <a href="https://mail.google.com/mail/u/0/#inbox?compose=new">
-                nirudikshi27@gmail.com
-              </a>
-            </td>
-          </tr>
-          <tr>
-            <td>Sushanth S </td>
-            <td>
-              <a href="https://mail.google.com/mail/u/0/#inbox?compose=new">
-                raosushanth511@gmail.com
-              </a>
-            </td>
-          </tr>
-          <tr>
-            <td>Yogesh K M </td>
-            <td>
-              <a href="https://mail.google.com/mail/u/0/#inbox?compose=new">
-                murthyogesh@gmail.com
-              </a>
-            </td>
-          </tr>
-        </table> */}
-      {/* </div> */}
+   
     </div>
   );
 };
